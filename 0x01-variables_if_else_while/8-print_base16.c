145163cca6aa2b65485a40beb97e9239d392f041
Erdey <erdeysyoum@gmail.com>
2023-08-11 11:10:13 -0700

    Hexadecimal Symbols

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int i, new_line;
+char c, char_line;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+c = (char)i;
+putchar(c);
+}
+new_line = 10;
+char_line = (char)new_line;
+putchar(char_line);
+return (0);
+}
