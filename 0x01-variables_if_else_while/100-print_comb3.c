bc2a51f1215e5429b36719c37fa6369b48bf9628
Erdey <erdeysyoum@gmail.com>
2023-08-12 07:04:57 -0700

    Nested forloop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int row, col;
+for (row = 48; row <= 57; ++row)
+{
+for (col = 48; col <= 57; ++col)
+{
+if (!(row == col) && col > row)
+{
+putchar(row);
+putchar(col);
+if (!(row == 56 && col == 57))
+putchar(',');
+} 
+}
+
+}
+putchar('\n');
+return (0);
+}
