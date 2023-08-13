df364c54fba58f02f9af9840c3ab47300ea53a40
Erdey <erdeysyoum@gmail.com>
2023-08-12 18:38:27 -0700

    Unique Sequence v4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,25 +5,32 @@
  *
  * Return: always 0 (Success)
  */
-int main(void) {
-  int row, col, k;
-  for (row = 48; row <= 57; ++row) {
-    for (col = 48; col <= 57; ++col) {
-      for (k = 48; k <= 57; ++k) {
-        if (!(col == k) && k > col) {
-          if (!(row == col) && col > row) {
-            putchar(row);
-            putchar(col);
-            putchar(k);
-            if (!(row == 55 && col == 56 && k == 57)) {
-              putchar(',');
-              putchar(' ');
-            }
-          }
-        }
-      }
-    }
-  }
-  putchar('\n');
-  return (0);
+int main(void) 
+{
+int row, col, k;
+for (row = 48; row <= 57; ++row)
+{
+for (col = 48; col <= 57; ++col) 
+{
+for (k = 48; k <= 57; ++k) 
+{
+if (!(col == k) && k > col)
+{
+if (!(row == col) && col > row) 
+{
+putchar(row);
+putchar(col);
+putchar(k);
+if (!(row == 55 && col == 56 && k == 57)) 
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
