51218a31a83c63c87171406976345fa8f1c56b0c
erdey2 <50736447+erdey2@users.noreply.github.com>
2023-08-13 05:21:34 +0300

    Generating Unique Sequence

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,30 +6,30 @@
  */
 int main(void) 
 {
-int row, col, k;
-for (row = 48; row <= 57; ++row)
-{
-for (col = 48; col <= 57; ++col) 
-{
-for (k = 48; k <= 57; ++k) 
-{
-if (!(col == k) && k > col)
-{
-if (!(row == col) && col > row) 
-{
-putchar(row);
-putchar(col);
-putchar(k);
-if (!(row == 55 && col == 56 && k == 57)) 
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+  int row, col, k;
+  for (row = 48; row <= 57; ++row)
+  {
+    for (col = 48; col <= 57; ++col) 
+    {
+      for (k = 48; k <= 57; ++k) 
+      {
+        if (!(col == k) && k > col)
+        {
+          if (!(row == col) && col > row) 
+          {
+            putchar(row);
+            putchar(col);
+            putchar(k);
+            if (!(row == 55 && col == 56 && k == 57)) 
+            {
+              putchar(',');
+              putchar(' ');
+            }
+          }
+        }
+      }
+    }
+  }
+  putchar('\n');
+  return (0);
 }
