3964ea5496af546683ddee1673638183d0f02a63
Erdey <erdeysyoum@gmail.com>
2023-08-12 17:59:47 -0700

    Unique Sequence

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,14 +14,13 @@ for (col = 48; col <= 57; ++col)
 {
 for (k = 48; k <= 57; ++k)
 {
-if(!(col == k) && k > col)
+if (!(col == k) && k > col)
 {
 if (!(row == col) && col > row)
 {
 putchar(row);
 putchar(col);
 putchar(k);
-
 if (!( row == 55 && col == 56 && k == 57))
 {
 putchar(',');
