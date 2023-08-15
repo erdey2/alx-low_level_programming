51dacd364b0c70b195a864a90c7c7f7e92f4f6d6
Erdey <erdeysyoum@gmail.com>
2023-08-15 14:56:48 -0700

    Addition Function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - Prints the sum of two numbers
+* @a: - the ist number to be summed
+* @b: - the 2nd number to be summed
+* Return: the sum of a and b
+*/
+int add(int a, int b)
+{
+	int result;
+	result = a + b;
+	return result;
+}
+
