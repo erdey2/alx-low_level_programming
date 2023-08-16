9df3123f8a2cf4d495b8f0f7d7a988f802dcb23b
Erdey <erdeysyoum@gmail.com>
2023-08-15 23:16:12 -0700

    print a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,5 +8,14 @@ void print_alphabet_x10(void)
 int i;
 for (i = 97; i <= 122; i++)
 _putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
+_putchar(i);
 _putchar('\n');
 }
