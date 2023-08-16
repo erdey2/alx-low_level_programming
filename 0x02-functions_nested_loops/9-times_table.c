ff34519fa714501a86faee2082a3199428104a1d
Erdey <erdeysyoum@gmail.com>
2023-08-16 02:45:49 -0700

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 	int i, j, k;
 
 	for (i = 0; i < 10; i++)
-	{
+	{	
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
@@ -20,15 +20,15 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-			       	_putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar((k / 10) + '0');
-                                _putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
