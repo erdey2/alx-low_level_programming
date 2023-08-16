d0c2dd5efc22be98361e6e00f8fe335a50b1e4d5
Erdey <erdeysyoum@gmail.com>
2023-08-15 23:09:31 -0700

    Print a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet - Make the alphabet
- * 
+ * print_alphabet_x10 - Print a-z 10 times
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	for (i = 97; i <= 122; i++)
-	       _putchar(i);
-	_putchar('\n');
+int i;
+for (i = 97; i <= 122; i++)
+_putchar(i);
+_putchar('\n');
 }
