afe89a6c405bcb26d74ccaf4f35bddd3124d3e6b
Erdey <erdeysyoum@gmail.com>
2023-08-16 00:08:35 -0700

    A program to display whether a letter is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _islower - Display whether a given character is lowercase or not
- * @c: - the single letter to be checked 
- * Return: 1 if the letter is in lowercase 0 otherwise
- */
+* _islower - Display whether a given character is lowercase or not
+* @c: - the single letter to be checked
+* Return: 1 if the letter is in lowercase 0 otherwise
+*/
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
