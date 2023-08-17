e1fde9e86ba01d168593fcfd66fdd22621a4e575
Erdey <erdeysyoum@gmail.com>
2023-08-17 05:32:09 -0700

    Print Most Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * print_most_numbers - prints mostly used numbers
- * Return: void
- */
-void print_most_numbers()
+* print_most_numbers - prints mostly used numbers
+* Return: void
+*/
+void print_most_numbers(void)
 {
 	int count;
-	
+
 	for (count = 48; count <= 57; count++)
 	{
 		if (count != 50 && count != 52)
 			_putchar(count);
 	_putchar('\n');
 	}
-
 }
