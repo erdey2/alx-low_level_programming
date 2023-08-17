45a0a5f1b78943ce42755954fba17ff5f3af3b17
Erdey <erdeysyoum@gmail.com>
2023-08-17 15:50:45 -0700

    List largest prime of a large number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point It prints the prime factor of  a large number
+* Return: the largest prime factor
+*/
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+	
+	for (i = 3; i < 782849; i += 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
