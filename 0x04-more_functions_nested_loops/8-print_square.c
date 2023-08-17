1cdd88f0e10bb5d8be27444f5186cbb8ca250339
Erdey <erdeysyoum@gmail.com>
2023-08-17 08:02:09 -0700

    Prind a given number of squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* print_square - prints a rectangle to the terminal a given number of times
+* @size: the size of the square to be drawn
+* Return: void
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+
+	}
+
+}
