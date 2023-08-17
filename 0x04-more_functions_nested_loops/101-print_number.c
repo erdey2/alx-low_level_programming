88e13b889cf907529087d38be8f973cd58ca4ca6
Erdey <erdeysyoum@gmail.com>
2023-08-17 15:12:04 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,15 @@
 void print_number(int n)
 {
 	unsigned int k = n;
+
 	if (n < 0)
 	{
 		n = n * -1;
 		k = n;
 		_putchar('-');
-		
 	}
 	k /= 10;
 	if (k != 0)
 		print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
-		
 }
