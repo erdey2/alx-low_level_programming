33aceec3f88e0fe4d361a542a006483a295ceebb
Erdey <erdeysyoum@gmail.com>
2023-08-16 18:40:02 -0700

    A given number of Multiplication Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,15 +30,12 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				else if (k > 99)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
@@ -48,8 +45,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('0');
-	}	
 }
