3e2a439c90dcace59fb611067dad0b849ac4da9f
Erdey <erdeysyoum@gmail.com>
2023-08-17 15:03:00 -0700

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_number - prints a number to terminal
+* @n: the number to be drawn
+* Return: void
+*/
+void print_number(int n)
+{
+	unsigned int k = n;
+	if (n < 0)
+	{
+		n = n * -1;
+		k = n;
+		_putchar('-');
+		
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+		
+}
