228dddbe56c2546a8e5342381c0c1a25681eb983
Erdey <erdeysyoum@gmail.com>
2023-08-18 03:29:35 -0700

    Number and string Collection

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0)
+		{
+			printf("Fizz ");
+		} else if (count % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", count);
+
+	}
+	printf("\n");
+	return (0);
+}
