13c6e7bf3dfe13111c244b08793858c419af2dcf
Erdey <erdeysyoum@gmail.com>
2023-08-19 01:45:25 -0700

    Number and Text Combination

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,27 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count % 3 == 0 && count % 5 == 0)
+		if (count != 100)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (count % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (count % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-			printf("%d ", count);
 
+			if (count % 3 == 0 && count % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (count % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (count % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+				printf("%d ", count);
+		} else
+		{
+			printf("Buzz");
+		}
 	}
 	printf("\n");
 	return (0);
