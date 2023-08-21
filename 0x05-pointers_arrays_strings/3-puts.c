279c2958de01f0a7523235272b06dcf96253bf2b
Erdey <erdeysyoum@gmail.com>
2023-08-21 02:41:11 -0700

    Display Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - display a given string to the output standard
+ * @str: the string to be desplayed
+ * Return: display the entered string
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\n')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar('\n');
+}
