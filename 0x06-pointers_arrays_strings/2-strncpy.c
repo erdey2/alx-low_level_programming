821e259c5aff4d566211c32496f88292e1e07572
Erdey <erdeysyoum@gmail.com>
2023-08-24 00:59:31 -0700

    Copy n size String

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	char *temp = dest;
 
-	while (dest[i] != '\0' && src[i] && i < n)
+	while (i < n && src[i] != '\0')
 	{
-		dest[i] = src[i];
+		temp[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
