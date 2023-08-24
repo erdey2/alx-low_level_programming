8a2558d56036e329466fb9b19b84595d3a20d0b1
Erdey <erdeysyoum@gmail.com>
2023-08-24 03:37:11 -0700

    Display an array in Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - Display an array in reverse order
+ * @a: the array to be displayed in reverse
+ * @n: number of elements to reversed
+ *  Return: the reversed array
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, i;
+	
+	for (i = 0; i < n--; i++)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+}
+
