38dc6fcb591aa6a5a565f4626ffd1d17fb2a8ef4
Erdey <erdeysyoum@gmail.com>
2023-08-24 07:28:10 -0700

    Change String in lowercase after space to Uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * *cap_string - Changes lowercase separated by space tabulation new line to uppercase
+ * *cap_string - Changes lowercase separated by space to uppercase
  * @n: the string to be changed
  *  Return: the changed strings
  */
 char *cap_string(char *n)
 {
 	int i = 0;
-	
+
 	while (n[i] != '\0')
 	{
 		while (!(n[i] >= 97 && n[i] <= 122))
 			i++;
-		if (n[i - 1] == ' ' || 
+		if (n[i - 1] == ' ' ||
 				n[i - 1] == '\t' || 
 				n[i - 1] == '\n' ||
 				n[i - 1] == ',' ||
@@ -32,4 +32,3 @@ char *cap_string(char *n)
 	}
 	return (n);
 }
-
