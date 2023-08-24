df16ed968d4a8c5bbd6801205f34d3a97ce92e84
Erdey <erdeysyoum@gmail.com>
2023-08-24 07:37:10 -0700

    Change String in lowercase after space to Uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,34 @@
 #include "main.h"
 /**
- * *cap_string - Changes lowercase separated by space to uppercase
- * @n: the string to be changed
- *  Return: the changed strings
- */
+* *cap_string - Changes lowercase separated by space to uppercase
+* @n: the string to be changed
+*  Return: the changed strings
+*/
 char *cap_string(char *n)
 {
-	int i = 0;
+int i = 0;
 
-	while (n[i] != '\0')
-	{
-		while (!(n[i] >= 97 && n[i] <= 122))
-			i++;
-		if (n[i - 1] == ' ' ||
-				n[i - 1] == '\t' || 
-				n[i - 1] == '\n' ||
-				n[i - 1] == ',' ||
-				n[i - 1] == '.' ||
-				n[i - 1] == ':' ||
-				n[i - 1] == '?' ||
-				n[i - 1] == ';' ||
-				n[i - 1] == '!' ||
-				n[i - 1] == '"'||
-				n[i - 1] == '(' ||
-				n[i - 1] == ')' ||
-				n[i - 1] == '{' ||
-				n[i - 1] == '}' ||
-				i == 0)
-			n[i] -= 32;
-		i++;
-	}
-	return (n);
+while (n[i] != '\0')
+{
+while (!(n[i] >= 97 && n[i] <= 122))
+i++;
+if (n[i - 1] == ' ' ||
+n[i - 1] == '\t' ||
+n[i - 1] == '\n' ||
+n[i - 1] == ',' ||
+n[i - 1] == '.' ||
+n[i - 1] == ':' ||
+n[i - 1] == '?' ||
+n[i - 1] == ';' ||
+n[i - 1] == '!' ||
+n[i - 1] == '"' ||
+n[i - 1] == '(' ||
+n[i - 1] == ')' ||
+n[i - 1] == '{' ||
+n[i - 1] == '}' ||
+i == 0)
+n[i] -= 32;
+i++;
+}
+return (n);
 }
