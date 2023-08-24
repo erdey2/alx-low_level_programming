2ef5646a8a1cdc67dd8ba289d1be60273c09504d
Erdey <erdeysyoum@gmail.com>
2023-08-24 04:30:08 -0700

    Lower to Uppercase Conversion

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - Changes lowercase to uppercase
+ * @n: the string to be changed
+ *  Return: strings in uppercase
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 97 && n[i] <= 122)
+			n[i] -= 32;
+		i++;
+	}
+	return n;
+}
+
