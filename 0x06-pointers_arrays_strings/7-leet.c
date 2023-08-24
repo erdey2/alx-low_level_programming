59f9b367eeda1af23b79b78b2583b8d87a33f724
Erdey <erdeysyoum@gmail.com>
2023-08-24 08:39:27 -0700

    Replace String with Numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *leet - Replace characters with numbers
+ * @n: the string to be changed
+ *  Return: the changed strings
+ */
+char *leet(char *n)
+{
+	int i, j;
+
+	char str[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == str[j])
+			{
+				n[i] = num[j];
+
+			}
+			
+		}
+	}
+	return (n);
+}
+
+
