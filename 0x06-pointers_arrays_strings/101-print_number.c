e16c9b3e0fd77ec68313224d26b600c2c318b675
Erdey <erdeysyoum@gmail.com>
2023-08-25 02:22:52 -0700

    Print Large numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_number - prints a large number
+* @n: the number to be drawn
+* Return: void
+*/
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		k = n;
+		_putchar('-');
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
+}
