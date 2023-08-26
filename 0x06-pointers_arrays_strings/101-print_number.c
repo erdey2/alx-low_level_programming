34125cf4ed887b53f297b809ee064f48b7fcadc1
Erdey <erdeysyoum@gmail.com>
2023-08-26 06:05:49 -0700

    Display Large Number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,15 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
-	if (n == 0)
-		_putchar('0');
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
