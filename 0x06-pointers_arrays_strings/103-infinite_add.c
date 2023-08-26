46348e45831cec7a91abd38dfa79257d018b2f60
Erdey <erdeysyoum@gmail.com>
2023-08-26 05:05:10 -0700

    Print Buffer Content

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *infinite_add - adds two large numbers using buffer (a temporary area in main memory)
+ * *infinite_add - adds large numbers using buffer(a temporary area in main memory)
  * @n1: the first number
  * @n2: the second number
  * @r: the buffer
@@ -11,8 +11,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++);
-	for (j = 0; n2[j]; j++);
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
 	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
