4234afdcd62ebf42eb5bfa31b8bc6328675d1b0d
Erdey <erdeysyoum@gmail.com>
2023-08-26 05:40:11 -0700

    Display Large Numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
-* print_number - prints large numbers
-* @n: the number to be drawn
-* Return: void
-*/
+ * print_number - prints a large number
+ * @n: the number to be printed
+ * Return: the number
+ */
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		n = n * - 1;
 	}
-	if (n1 / 10 != 0)
-		print_number(n1 / 10);
-	_putchar((n1 % 10 + '0'));
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
