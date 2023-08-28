d31fea7fd3c2ec685c795f3ca83297f9b0a190c2
Erdey <erdeysyoum@gmail.com>
2023-08-28 01:57:10 -0700

    Memory Copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - Copies the block of n bytes
+ * @dest: copt to this
+ * @src: copy from
+ * @n: block size
+ * Return: the address of the copied
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+	
