9422a2f429bdc98a7ef3d267306d07a07afae573
Erdey <erdeysyoum@gmail.com>
2023-08-28 11:55:09 -0700

    Double Pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Sets a string
+ * @s: the string to be set
+ * @to: the the string to be replaced
+ * Return:The replaced string
+ */
+void set_string(char **ss, char *to)
+{
+	*s = to;
+}
