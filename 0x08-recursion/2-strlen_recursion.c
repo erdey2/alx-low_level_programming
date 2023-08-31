5b5126787af634a2cf65ecda4d9cd79b99ada489
Erdey <erdeysyoum@gmail.com>
2023-08-31 05:28:41 -0700

    Return length of a string Recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - display the length of a string recorsevely
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length = length + _strlen_recursion(s + 1);
+	}
+	return length;
+}
