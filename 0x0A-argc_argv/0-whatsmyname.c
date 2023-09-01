0bb12f59e873d0a762c8769e0aa76199e9746341
Erdey <erdeysyoum@gmail.com>
2023-09-01 06:23:50 -0700

    Main Arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  * @argv: argument 2
  * Return: void
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	
-	return 0;
+
+	return (0);
 }
