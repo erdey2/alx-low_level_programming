fdfd0bd6ab99d0f0212be17947cdb9a22be847cc
Erdey <erdeysyoum@gmail.com>
2023-09-01 08:52:33 -0700

    Multply Numbers using command line arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
 		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
