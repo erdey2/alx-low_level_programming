0bb00072b6b0655f3a35d99a256f4d612eda049c
Erdey <erdeysyoum@gmail.com>
2023-09-01 06:49:30 -0700

    Print All Arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument 1
+ * @argv: argument 2
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+
+	}
+
+	return (0);
+}
