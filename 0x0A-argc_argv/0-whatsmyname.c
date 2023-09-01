7c849eb136b07cc395f0bbba0f23591f66e9d1c2
Erdey <erdeysyoum@gmail.com>
2023-09-01 06:20:08 -0700

    Main Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument 1
+ * @argv: argument 2
+ * Return: void
+ */
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	
+	return 0;
+}
