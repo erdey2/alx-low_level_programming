0a4fe2e0d57aa86d66b43eb5356638efa6ca9ff2
Erdey <erdeysyoum@gmail.com>
2023-09-02 01:36:09 -0700

    sum of positive numbers

---
+++ 0x0A-argc_argv/4-add2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument 1
+ * @argv: argument 2
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0, n, numbers[100];
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else if (argc >= 3)
+	{
+		for (n = 1; n < argc; n++)
+		{
+			if (atoi(argv[n]) > 0)
+			{
+				numbers[n] = atoi(argv[n]);
+				result += numbers[n];
+			}
+			else if (atoi(argv[n]) == 0)
+			{
+				if (!(*argv[n] >= 48 || *argv[n] <= 57))
+				{
+					printf("0\n");
+				}
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
