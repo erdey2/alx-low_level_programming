746f67868588e7b425014aa80b78d02ee37197ed
Erdey <erdeysyoum@gmail.com>
2023-09-04 03:23:52 -0700

    create and return dynamic memory address

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - creates a new space in memory and return the addresss of that
+ * @str: the address of the string to be retured
+ * Return: the address of a newly created space in memory
+ */
+
+char *_strdup(char *str)
+{
+	char *p, *q;
+	unsigned int count = 0, n;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	p = malloc((count * sizeof(char)) + 1);
+	q = p;
+
+	if (p != NULL)
+	{
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (n = 0; n < count; n++)
+			{
+				*(q + n) = *(str + n);
+			}
+			*(q + count) = '\0';
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (p);
+}
