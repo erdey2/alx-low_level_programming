b65ea1587a6f8758b92cb0814d6c62b87c6f27d0
Erdey <erdeysyoum@gmail.com>
2023-09-04 07:42:53 -0700

    string concatenate using dynamic allocated memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,31 +10,32 @@
 
 char *str_concat(char *s1, char *s2)
 {
-        unsigned int i = 0, j = 0, len = 0;
-        char *new_str;
+	unsigned int i = 0, j = 0, len = 0;
+	char *new_str;
 
-        if (s1 == NULL && s2 == NULL)
-                return (NULL);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL && s2 != NULL)
+		return (s2);
+	if (s2 == NULL)
+	{
+		new_str = s1;
+		return (new_str);
+	}
+	while (s2[len] != '\0')
+	{
+		len++;
+	}
 
-        if (s2 == NULL)
-        {
-                new_str = s1;
-                return (new_str);
-        }
+	new_str = malloc(s1, (len + 1) * sizeof(char));
 
-        while (s2[len] != '\0')
-        {
-                len++;
-        }
-        new_str = malloc((len + 1) * sizeof(char));
-        if (new_str == NULL)
-                return (NULL);
+	if (new_str == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		new_str[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		new_str[i + j] = s2[j];
+	new_str[i + j] = '\0';
 
-        for (i = 0; s1[i] != '\0'; i++)
-                new_str[i] = s1[i];
-        for (j = 0; s2[j] != '\0'; j++)
-                new_str[i + j] = s2[j];
-        new_str[i + j] = '\0';
-
-        return (new_str);
+	return (new_str);
 }
