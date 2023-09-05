35f3a49c2e2821133926d94deca103175daf348e
Erdey <erdeysyoum@gmail.com>
2023-09-05 04:50:50 -0700

    Dynamic Memory allocate for 2-d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -37,10 +37,9 @@ int **alloc_grid(int width, int height)
     for (i = 0; i < width; i++)
     {
         for (j = 0; j < height; j++)
-        {
-            p[i][j] = 0;
-       }
+		p[i][j] = 0;
+	free(p[i]);
     }
-
+    free(p);
     return p;
 }
