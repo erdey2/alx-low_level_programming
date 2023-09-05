40d871e23722908d003755f8b9867f2d449cae1b
Erdey <erdeysyoum@gmail.com>
2023-09-05 05:43:24 -0700

    Free Memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - releases previosly created memory
+ * @grid: memory space
+ * @height: the number of rows
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
