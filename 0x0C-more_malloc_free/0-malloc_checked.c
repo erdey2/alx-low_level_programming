66aa70bd805383aca879ecec21e909e7bef3f67f
Erdey <erdeysyoum@gmail.com>
2023-09-07 02:17:20 -0700

    Memory allocate and exit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * mallo_checked - allocates a memory and assigns it a pointer 
+ * malloc_checked - allocates a memory and assigns it a pointer
  * @b: the size
  * Return: the pointer to the assigned space
  */
@@ -10,7 +10,7 @@
 void *malloc_checked(unsigned int b)
 {
 	void *p;
-	
+
 	p = malloc(b);
 	if (p == NULL)
 	{
