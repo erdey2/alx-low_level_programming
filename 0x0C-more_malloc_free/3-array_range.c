b8a74ccb6fff78e89514c7cde6b8dd3b8c228de0
Erdey <erdeysyoum@gmail.com>
2023-09-07 05:58:59 -0700

    array range displayed in sorted order

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * * @min: the minimum number
+ * * @max: the maximum number
+ * Return: the sorted array
+ */
+
+int  *array_range(int min, int max)
+{
+        int *p, i, count = 0;
+
+        if (min > max)
+        {
+                return (NULL);
+        }
+        for (i = min; i <= max; i++)
+                count++;
+        p = calloc(count, sizeof(int));
+        if (p == NULL)
+        {
+                return (NULL);
+
+        }
+        for (i = min; i <= max; i++)
+                p[i] = min++;
+        return (p);
+}
