0fb7708b178a68a30cb9bd98b033e40fa0ab48da
Erdey <erdeysyoum@gmail.com>
2023-09-11 06:43:59 -0700

    Create a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
 #ifndef DOG
 #define DOG
+
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
 struct dog
 {
 	char *name;
