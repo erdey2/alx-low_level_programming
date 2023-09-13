a45f71ff6f7168539a4c7d4f82a7446023c6487e
Erdey <erdeysyoum@gmail.com>
2023-09-13 04:47:32 -0700

    Adding an array using function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,13 +10,13 @@
 
 void array_iterator(int *array, size_t size, void (*action) (int))
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (array != NULL && action != NULL)
 	{
 		while (i < size)
 		{
-			f(array[i]);
+			action(array[i]);
 			i++;
 		}
 	}
