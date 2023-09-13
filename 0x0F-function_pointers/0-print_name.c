d30eaa2d0e98291acee020fb3476b9378d7e4738
Erdey <erdeysyoum@gmail.com>
2023-09-13 01:31:59 -0700

    a function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - welcome by name
+ * @name: the name of a person
+ * @f: function 
+ * Return: void
+ *
+ */
+
+void print_name(char *name, void (*f) (char *))
+{
+	f(name);
+}
+
