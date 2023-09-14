21e209ee4e3cbfcee05dcd50879bce3a7f7ee722
Erdey <erdeysyoum@gmail.com>
2023-09-14 03:47:15 -0700

    Print variable num arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,21 +14,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arglist;
 
 	va_start(arglist, n);
-	if (separator != NULL)
+	if (separator == NULL)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-			{
-				printf("%d %s", va_arg(arglist, int), separator);
-			}
-			else
-			{
-				printf("%d", va_arg(arglist, int));
-			}
-		}
-		printf("\n");
-		va_end(arglist);
+		separator = "";
 	}
-	separator = "";
+	for (i = 0; i < n; i++)
+	{
+		if (i >= n - 1)
+		{
+			printf("%d %s", va_arg(arglist, int), separator);
+		}
+		else
+		{
+			printf("%d", va_arg(arglist, int));
+		}
+	}
+	printf("\n");
+	va_end(arglist);
 }
