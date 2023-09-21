4a5f3352fcd5e172650ca0505713a43874209fe3
erdey2 <erdeysyoum@gmail.com>
2023-09-21 02:50:03 -0700

    Display and count the nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the information of the linked list
+ * @h: the address of the node to be printed
+ * 
+ * Return: the size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h -> str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h -> next;
+	}
+	return (count);
+}
