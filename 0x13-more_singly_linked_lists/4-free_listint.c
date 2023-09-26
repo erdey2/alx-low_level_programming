cd695d8b575276f4cde73d2eec8e911f78688737
erdey2 <erdeysyoum@gmail.com>
2023-09-26 02:34:25 -0700

    Free Allocated Space

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - free an allocated memory
+ * @head: the address of the first node
+ *
+ *Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free (head);
+	       head = next;
+	}
+}
