6278f77545900d1cabb49fb77a2c3f02f0a7f953
erdey2 <erdeysyoum@gmail.com>
2023-09-26 02:50:10 -0700

    Free Memory using double pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,13 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return;
-	else 
+	else
+	{	
 		while (*head != NULL)
 		{
-			next = (*head->next);
+			next = (*head)->next;
 			free(*head);
 			*head = next;
 		}
+	}
 }
