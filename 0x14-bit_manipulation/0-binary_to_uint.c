e9bf5292202d09ac5f16894445fc32fccf56daf5
erdey2 <erdeysyoum@gmail.com>
2023-09-29 07:12:09 -0700

    Convert binary to sign integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,9 +14,14 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		       return (0);	
+	}
 	while (b[i] == '0' || b[i] == '1')
 	{
-		val = val<< 1;
+		val = val << 1;
 		val += b[i] - '0';
 		i++;
 	}
