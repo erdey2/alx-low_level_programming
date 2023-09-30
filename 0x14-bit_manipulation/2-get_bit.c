44a7cdbd8c1b4c4ec1abf2d03356cf6ecb0fa039
erdey2 <erdeysyoum@gmail.com>
2023-09-30 06:05:46 -0700

    Find value of particular bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
  * @n: number of bits
  * @index: the position of the bit
  *
- * Return: value of the bit 
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	mask = 1 << index;
-	res = n & div;
+	res = n & mask;
 	if (res == mask)
 		return (1);
 
