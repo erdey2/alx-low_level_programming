6f5b6c2d870fddcbf7e51aaafaf9677fa20adb75
erdey2 <erdeysyoum@gmail.com>
2023-09-30 07:09:52 -0700

    Turn a bit off using bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	i = i << index;
+	i = ~(1 << index);
 	*n = *n & i;
 
 	return (1);
