52af6c8b570386d17374aebc8907296c363d8d5e
erdey2 <erdeysyoum@gmail.com>
2023-09-30 07:01:12 -0700

    Set a bit in a particular index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int i;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-			return (-1);
+		return (-1);
 	i = 1 << index;
 	*n = *n | i;
 
