ecac7adcd67018879feb4bb90f40661b67e8e3fb
erdey2 <erdeysyoum@gmail.com>
2023-10-02 08:14:11 -0700

    Append text at the end

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _strlen - calculate the string length
+ * @str: the string we want its length
+ *
+ *Return: the size
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return len;
+}
+
+/**
  * append_text_to_file - add new text at end
  * @filename: - the name of the file
  * @text_content - the new content to be added
@@ -20,7 +37,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	if (text_content != NULL)
-		len = write(fd, text_content, strlen(text_content));
+		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (len < 0)
 		return (-1);
