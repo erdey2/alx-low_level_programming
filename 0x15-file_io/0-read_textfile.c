058911447dfa5953d729b6b5012dc9de12e462f2
erdey2 <erdeysyoum@gmail.com>
2023-10-02 06:57:21 -0700

    Read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,13 +11,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, sz;
 	char *c = malloc(sizeof(char) * letters);
+	if (c == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
+	fd = open(filename, O_RONLY);
 	sz = read(fd, c, letters);
+	if (fd < 0 || sz < 0)
+	{
+		free(c);
+		return (0);
+	}
 	c[sz] = '\0';
 	printf("%s\n", c);
 	return (sz);
