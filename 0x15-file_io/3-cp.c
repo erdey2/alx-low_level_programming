c32862c77d61a93c7137d6dcc2d5b86faa05d21d
erdey2 <erdeysyoum@gmail.com>
2023-10-03 01:05:37 -0700

    Copy content from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,27 +9,29 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int in_fd, out_fd;
 	int num1 = BUFSIZE, num2 = 0;
 	char buf[BUFSIZE];
-	mode_t file_perm;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp in_fd out_fd\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp in_fd out_fd\n");
+		exit(97);
+	}
 	in_fd = open(argv[1], O_RDONLY);
 	if (in_fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	out_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, file_perm);
+	out_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (out_fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(in_fd), exit(99);
+		close(in_fd);
+		exit(99);
 	}
 	while (num1 == BUFSIZE)
 	{
@@ -41,12 +43,21 @@ int main(int argc, char **argv)
 		}
 		num2 = write(out_fd, buf, num1);
 		if (num2 < num1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	if (close(in_fd) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", in_fd), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", in_fd);
+		exit(100);
+	}
 	if (close(out_fd) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", out_fd), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", out_fd);
+		exit(100);
+	}
 
 	return (0);
 }
