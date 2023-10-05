5dff5368e6353199e008ce5b06ecedc04ff4a9d7
erdey2 <erdeysyoum@gmail.com>
2023-10-05 06:57:01 -0700

    Low level File Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,44 +20,37 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	input_fd = open(argv[1], O_RDONLY);
 	if (input_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 	}
 	open_flags = O_CREAT | O_WRONLY | O_TRUNC;
 	out_fd = open(argv[2], open_flags, 0664);
 	if (out_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	while ((num_read = read(input_fd, buf, BUFSIZE)) > 0)
 	{
 		if (write(out_fd, buf, num_read) != num_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
 	if (num_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(input_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", input_fd), exit(100);
 	}
 	if (close(out_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", out_fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", out_fd), exit(100);
 	}
 	return (0);
 
