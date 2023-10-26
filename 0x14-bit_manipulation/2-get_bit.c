4016abdf40111fdbad587df7c181bd345b0c9bac
Erdey <erdeysyoum@gmail.com>
2023-10-26 03:42:46 -0700

    get bit of a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,13 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n & 1 << index)
+	if (n & (1 << index) == 1)
 	{
 		return (1);
 	}
-	else
+	else if (n & (1 << index) == 0)
 		return (0);
+	else
+		return (-1);
 
 }
