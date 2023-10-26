2fbe4c6e415f1d72a82893796eb7af36a4d090d8
Erdey <erdeysyoum@gmail.com>
2023-10-26 06:32:02 -0700

    set particular bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,9 +12,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int *new;
 
 	new = n;
-	*new |= (1 << index);
-	if (index > 64)
+	
+	if (n == NULL)
 		return (-1);
+	*new |= (1 << index);
 	if (*new)
 		return (1);
 	else
