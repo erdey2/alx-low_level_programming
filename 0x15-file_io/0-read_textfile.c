3bb6a80b9a19533f8bb1b0f2891f196798847650
Erdey <erdeysyoum@gmail.com>
2023-10-30 04:45:40 -0700

    read and write to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,21 +20,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	fd1 = open(filename, O_RDONLY);
-	if (fd1 == -1)
+	nread = read(fd1, buf, letters);
+	nwrite = write(STDOUT_FILENO, buf, nread);
+	if (fd1 == -1 || nread == -1 || nwrite == -1 || nwrite != nread)
 	{
+		free(buf);
 		return (0);
-	}
-	while ((nread = read(fd1, buf, letters)) > 0)
-	{
-		if (nread == -1)
-			return (-1);
-		nwrite = write(STDOUT_FILENO, buf, nread);
-		if (nwrite == -1 || nwrite != nread)
-			return (0);
 	}
 	free(buf);
-	if (close(fd1) == -1)
-		return (0);
+	close(fd1);
 	return (nwrite);
 }
