8bbff96d2f221204e78c23b774ecf114f9c1287b
Erdey <erdeysyoum@gmail.com>
2023-10-30 06:33:13 -0700

    Append text at the end

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,8 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY, O_APPEND);
-	if (text_content != NULL)
-		nwrite = write(fd, text_content, len);
+	nwrite = write(fd, text_content, len);
 	if (fd == -1 || nwrite == -1)
 		return (-1);
 	close(fd);
