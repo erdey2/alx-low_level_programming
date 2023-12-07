c7d146cf9e8165ff7dbaeb1dcd29d902dd0e6563
Erdey <erdeysyoum@gmail.com>
2023-12-07 05:29:40 -0800

    sum the node data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums the node's data
+ * @head: the address of the first node
+ *
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	size_t sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
