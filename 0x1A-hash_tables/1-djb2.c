7dd83b93903c9275879146930129673a90c571d0
Erdey <erdeysyoum@gmail.com>
2023-12-21 07:28:31 -0800

    compute an index

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - a hash function
+ * @str - the input string 
+ *
+ * Return - an index into the table
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash_value;
+	int c;
+
+	hash_value = 5381;
+	while ((c = *str++))
+		hash_value = ((hash_value << 5) + hash_value) + c;
+	return (hash_value);
+
+}
