1c50637f4e8bd7fd57d05a451d74a15bdeb350b9
Erdey <erdeysyoum@gmail.com>
2023-12-22 06:09:59 -0800

    Display Hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the hash table
+ * @ht: the hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int index = 0;
+	char *comma = "";
+
+	if (!ht || !ht->array)
+		return;
+	putchar('{');
+	while (index < ht->size)
+	{
+		temp = ((ht->array)[index]);
+		while (temp)
+		{
+			printf("%s '%s': '%s'", comma, temp->key, temp->value);
+			comma = ", ";
+			temp = temp->next;
+		}
+		index++;
+	}
+	puts("}");
+}
