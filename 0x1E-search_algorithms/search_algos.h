ad37b6f4a6fd6bdbd1f453f77c4f02d408448e2f
erdey2 <erdeysyoum@gmail.com>
2024-04-09 02:58:14 -0700

    header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
